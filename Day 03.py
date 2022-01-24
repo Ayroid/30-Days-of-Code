0d5b8383780afc3328375825d7bc4b07c6296d74
Ayush Singh Kushwah <96888236+Ayroid@users.noreply.github.com>
2022-01-24 21:46:29 +0530

    Add files via upload

---
+++ Day 03.py
@@ -0,0 +1,21 @@
+from lib2to3.pgen2.token import GREATER
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    N = int(input().strip())
+
+if(N%2):
+    print("Weird")
+else:
+    if N in range(2,6) or N>20:
+        print("Not Weird")
+    elif N in range(6,21):
+        print("Weird")
+
+
